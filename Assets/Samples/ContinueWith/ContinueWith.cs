6487bd1378e1f1da64c98efb38295b394be9ecc9
SakaIT <it@sakaf.net>
2023-03-19 17:53:12 +0900

    [add]ContinueWithサンプルを作成

---
+++ Assets/Samples/ContinueWith/ContinueWith.cs
@@ -0,0 +1,26 @@
+﻿using UniRx;
+using UniRxVisualizeSamples.Common;
+using UnityEngine;
+
+namespace UniRxVisualizeSamples.Samples.ContinueWith
+{
+    public class ContinueWith : MonoBehaviour
+    {
+        [Header("Observable")] [SerializeField]
+        private SampleObservable observableSource = default;
+
+        [SerializeField] private SampleObservable observableOther = default;
+
+        [Header("Observer")] [SerializeField] private SampleObserver observer = default;
+
+        private void Start()
+        {
+            observableSource
+                .Observable
+                .ContinueWith(
+                    observableOther.Observable)
+                .Subscribe(observer)
+                .AddTo(this);
+        }
+    }
+}
